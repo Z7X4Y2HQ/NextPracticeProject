3a04bfe0584676d8b74d1cfa28746f46ffa95283
Z7X4Y2HQ <87140889+Z7X4Y2HQ@users.noreply.github.com>
2024-07-18 13:10:43 +0500

    initial push

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+import "@/app/ui/global.css";
+import fonts from "@/app/ui/fonts";
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={`${fonts.inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+const inter = Inter({ subsets: ['latin'] });
+const lusitana = Lusitana({weight: "400", subsets: ['latin']});
+const fonts = {inter, lusitana};
+export default fonts;
